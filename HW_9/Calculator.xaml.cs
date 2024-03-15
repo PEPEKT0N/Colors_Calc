11f96693ee825a17cce83e7f2763a3d027f87af1
PEPEKT0N <159730476+PEPEKT0N@users.noreply.github.com>
2024-03-15 14:50:06 +0300

    Add files via upload

---
+++ HW_9/Calculator.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HW_9
+{
+    /// <summary>
+    /// Логика взаимодействия для Calculator.xaml
+    /// </summary>
+
+
+    public partial class Calculator : Window
+    {
+
+        double num1 = 0.0;
+        double num2 = 0.0;
+        char op;
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void operation_click()
+        {
+            result_display.Text = display.Text;
+            display.Text = "0";
+        }
+        private void btn_one_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (display.Text == "0")
+                display.Text = btn.Content.ToString();
+            else
+                display.Text += btn.Content.ToString();
+            
+        }
+
+        private void btn_division_Click(object sender, RoutedEventArgs e)
+        {
+            operation_click();           
+            op = '/';           
+        }
+
+        private void btn_multiply_Click(object sender, RoutedEventArgs e)
+        {
+            operation_click();
+            op = '*';            
+        }
+
+        private void btn_minus_Click(object sender, RoutedEventArgs e)
+        {
+            operation_click();
+            op = '-';            
+        }
+
+        private void btn_plus_Click(object sender, RoutedEventArgs e)
+        {
+            operation_click();
+            op = '+';            
+        }
+
+        private void btn_result_Click(object sender, RoutedEventArgs e)
+        {
+            num2 = Double.Parse(display.Text);
+            switch (op) 
+            {
+                case '/':
+                    if (Double.Parse(display.Text) == 0.0)
+                    {
+                        MessageBox.Show("На ноль делить нельзя", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                        display.Text = "0";
+                    }
+                    else
+                        display.Text = (Double.Parse(result_display.Text) / Double.Parse(display.Text)).ToString();
+                    break;
+                case '*':
+                    display.Text = (Double.Parse(result_display.Text) * Double.Parse(display.Text)).ToString();
+                    break;
+                case '+':
+                    display.Text = (Double.Parse(result_display.Text) + Double.Parse(display.Text)).ToString();
+                    break;
+                case '-':
+                    display.Text = (Double.Parse(result_display.Text) - Double.Parse(display.Text)).ToString();
+                    break;
+                case '%':
+                    display.Text = (Double.Parse(result_display.Text) * Double.Parse(display.Text) / 100.0).ToString();
+                    break;
+            }
+            operation_click();
+        }
+
+        private void btn_sign_Click(object sender, RoutedEventArgs e)
+        {
+            display.Text = (Double.Parse(display.Text) * -1).ToString();
+        }
+
+        private void btn_clear_Click(object sender, RoutedEventArgs e)
+        {
+            display.Text = "0";
+            result_display.Text = "0";  
+        }
+
+        private void btn_dote_Click(object sender, RoutedEventArgs e)
+        {
+            if (display.Text.Contains(","))
+            {
+                return;
+            }
+            else display.Text += ",";
+
+        }
+
+        private void btn_percent_Click(object sender, RoutedEventArgs e)
+        {
+            operation_click();
+            op = '%';
+        }
+    }
+}
---
+++ HW_9/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }        
+        private void btm_navy_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.Navy;
+        }
+
+        private void btm_blue_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.Blue;
+        }
+
+        private void btm_aqua_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.Aqua;
+        }
+
+        private void btm_teal_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.Teal;
+        }
+
+        private void btm_olive_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.Olive;
+        }
+
+        private void btm_green_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.Green;
+        }
+
+        private void btm_Lime_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.Lime;
+        }
+
+        private void btm_yellow_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.Yellow;
+        }
+
+        private void btm_orange_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.Orange;
+        }
+
+        private void btm_red_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.Red;
+        }
+
+        private void btm_maroon_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.Maroon;
+        }
+
+        private void btm_fuchsia_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.Fuchsia;
+        }
+
+        private void btm_purple_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.Purple;
+        }
+
+        private void btm_black_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.Black;
+        }
+
+        private void btm_silver_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.Silver;
+        }
+
+        private void btm_gray_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.Gray;
+        }
+
+        private void btm_white_Click(object sender, RoutedEventArgs e)
+        {
+            main_grid.Background = Brushes.White;
+        }
+
+        private void btm_calc_Click(object sender, RoutedEventArgs e)
+        {            
+            Calculator calc = new Calculator();
+            calc.Show();
+        }
+    }
+}
